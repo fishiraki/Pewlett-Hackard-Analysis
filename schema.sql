3ff979e5d1798c97d4488c348670c8b0a5a19c51
fishiraki <fishiraki@gmail.com>
2020-08-25 20:02:42 -0700

    7.3.1

--- schema.sql
+++ schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE salaries (
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
 CREATE TABLE dept_employees (
@@ -38,6 +38,7 @@ CREATE TABLE dept_employees (
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE titles (
@@ -45,6 +46,6 @@ CREATE TABLE titles (
   title VARCHAR(40) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
 );
